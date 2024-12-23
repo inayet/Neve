c84bbc8cf832ff7705de2fce774a4af76dbb15dc
inayet <inayet@dreamsapi.com>
2024-12-23 02:32:38 -0700

    changed to lib.mkForce to ensure lazy-nvim is pluginmanager

--- config/pluginmanagers/default.nix
+++ config/pluginmanagers/default.nix
@@ -12,6 +12,6 @@
     pluginmanagers.enable = lib.mkEnableOption "Enable pluginmanagers module";
   };
   config = lib.mkIf config.pluginmanagers.enable {
-    lazy-nvim.enable = lib.mkDefault true;
+    lazy-nvim.enable = lib.mkForce true; # changed to lib.mkForce to ensure lazy-nvim is pluginmanager
   };
 }
